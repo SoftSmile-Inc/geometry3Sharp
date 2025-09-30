aa72cb0cb06f327083c938ea431a4ff5c83e1cdc
nevelskiy <anevel.85@gmail.com>
2025-09-30 17:48:50 +0300

    Add remove isolated vertices flag to RemoveTrianglesMeshChange

--- mesh/DMesh3Changes.cs
+++ mesh/DMesh3Changes.cs
@@ -194,18 +194,20 @@ namespace g3
 
         public Action<IEnumerable<int>,IEnumerable<int>> OnApplyF;
         public Action<IEnumerable<int>, IEnumerable<int>> OnRevertF;
+        protected bool RemoveIsolatedVertices = true;
 
         public RemoveTrianglesMeshChange()
         {
         }
 
 
-        public void InitializeFromApply(DMesh3 mesh, IEnumerable<int> triangles)
+        public void InitializeFromApply(DMesh3 mesh, IEnumerable<int> triangles, bool removeIsolatedVertices = true)
         {
+            RemoveIsolatedVertices = removeIsolatedVertices;
             initialize_buffers(mesh);
             bool has_groups = mesh.HasTriangleGroups;
 
-            foreach ( int tid in triangles ) { 
+            foreach ( int tid in triangles ) {
                 if (!mesh.IsTriangle(tid))
                     continue;
 
@@ -219,7 +221,7 @@ namespace g3
                 RemovedT.Add(tid);
                 Triangles.Add(tri);
 
-                MeshResult result = mesh.RemoveTriangle(tid, true, false);
+                MeshResult result = mesh.RemoveTriangle(tid, bRemoveIsolatedVertices: RemoveIsolatedVertices, bPreserveManifold: false);
                 if (result != MeshResult.Ok)
                     throw new Exception("RemoveTrianglesMeshChange.Initialize: exception in RemoveTriangle(" + tid.ToString() + "): " + result.ToString());
                 Util.gDevAssert(mesh.IsVertex(tv.a) == va && mesh.IsVertex(tv.b) == vb && mesh.IsVertex(tv.c) == vc);
@@ -228,8 +230,9 @@ namespace g3
 
 
 
-        public void InitializeFromExisting(DMesh3 mesh, IEnumerable<int> remove_t)
+        public void InitializeFromExisting(DMesh3 mesh, IEnumerable<int> remove_t, bool removeIsolatedVertices = true)
         {
+            RemoveIsolatedVertices = removeIsolatedVertices;
             initialize_buffers(mesh);
             bool has_groups = mesh.HasTriangleGroups;
 
@@ -270,7 +273,7 @@ namespace g3
             int N = RemovedT.size;
             for ( int i = 0; i< N; ++i) {
                 int tid = RemovedT[i];
-                MeshResult result = mesh.RemoveTriangle(tid, true, false);
+                MeshResult result = mesh.RemoveTriangle(tid, bRemoveIsolatedVertices: RemoveIsolatedVertices, bPreserveManifold: false);
                 if (result != MeshResult.Ok)
                     throw new Exception("RemoveTrianglesMeshChange.Apply: error in RemoveTriangle(" + tid.ToString() + "): " + result.ToString());
             }
